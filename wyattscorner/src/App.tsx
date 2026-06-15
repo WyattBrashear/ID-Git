275389a9d386df6acf46a2b0fa113aeeab98f77d
Wyatt B. <158331550+WyattBrashear@users.noreply.github.com>
2026-06-15 16:12:26 -0400

    Committing to the cloud

--- wyattscorner/src/App.tsx
+++ wyattscorner/src/App.tsx
@@ -1,10 +1,40 @@
-import { useState } from 'react'
 import './App.css'
 
-function App() {
-  return (
-    <p>Hi</p>
+
+async function query_ai() {
+  const response = await fetch('http://127.0.0.1:5000/api/query', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ messages: [{ role: 'user', content: 'Can i eat AI??' }], model: 'moonshotai/Kimi-K2.7-Code' }),
+  })
+  return await response.json()
+}
+function construct_navbar() {
+  return(
+  <div className="rounded">
+    <nav>
+      <ul>
+
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#funnies">Funnies</a></li>
+        <li><a href="#guestbook">Guestbook</a></li>
+      </ul>
+    </nav>
+  </div>
   )
 }
+
+function App() {
+  async function responseHandler() {
+    let response = await query_ai()
+    document.getElementById('response').textContent = response.response
+  }
+  return (
+    construct_navbar()
+  )
+}
+
 
 export default App
